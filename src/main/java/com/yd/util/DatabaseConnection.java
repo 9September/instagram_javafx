3b2189f1f2bc59d3244a24599160102f018418b8
9September <parkyoungdu@naver.com>
2024-11-26 22:02:31 +0900

    Database update

--- src/main/java/com/yd/util/DatabaseConnection.java
+++ src/main/java/com/yd/util/DatabaseConnection.java
@@ -8,7 +8,7 @@ public class DatabaseConnection {
 
     private static final String URL = "jdbc:mysql://localhost:3306/INSTARGRAM?allowPublicKeyRetrieval=true&useSSL=false&serverTimezone=UTC";
     private static final String USER = "root";
-    private static final String PASSWORD = "yd157238+";
+    private static final String PASSWORD = "12345";
 
     private static Connection connection = null;
 
